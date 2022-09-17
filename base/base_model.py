4fa37b3602d01ee72feebddf09ab2b922256cebd
Srinath Jayaraman <srinath.jayaraman92@gmail.com>
2022-09-17 21:52:12 +0200

    fixed minor formatting issues

--- base/base_model.py
+++ base/base_model.py
@@ -8,7 +8,6 @@ import torch.nn as nn
 import numpy as np
 from abc import abstractmethod
 
-
 class BaseModel(nn.Module):
     """
     Base class for all models
@@ -17,7 +16,6 @@ class BaseModel(nn.Module):
     def forward(self, *inputs):
         """
         Forward pass logic
-
         :return: Model output
         """
         raise NotImplementedError
